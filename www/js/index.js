dea267ac366e0649d2fcd6b45349819555092da6
Peter <Peter@59.18.234.89>
2014-03-31 01:40:11 +0900

    .

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,61 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+window.onload = function() {
+	"use strict";
+	var randomNumber = new Array(15);
+	var menteeMember = new Array(15);
+	var count = 0;
+	function shuffle() {
+		var chk = true;
+		for (var i = 0; i < 15; i++) {
+			var rand = parseInt(Math.random() * 15 + 1);
+			chk = true;
+			for (var j = 0; j < i; j++) {
+				if (randomNumber[j] == rand) {
+					i--;
+					chk = false;
+				}
+			}
+			if (chk) {
+				randomNumber[i] = rand;
+			}
+		}
+	}
+	$('.name').click(function(event) {
+		event.preventDefault();
+		if ($(this).parent().attr('id') == 'unSelected') {
+			if (($('#Selected > .name').length) < 15) {
+				$(this).appendTo('#Selected');
+				shuffle();
+			}
+		} else {
+			$(this).appendTo('#unSelected');
+		}
+	});
+	$('#procBtn').click(
+			function() {
+				if ($('#Selected > .name').length == 15) {
+					console.log('okok');
+					$('.mkTeam').css('display', '');
+					$('.sel').css('display', 'none');
+					$('#Selected > .name').each(
+							function(index, data) {
+								$('<div>').text($(data).text()).attr('class',
+										'name').appendTo('.mentorBox');
+							});
+					$('#unSelected > .name').each(function(index, data) {
+						menteeMember.push($(data).text());
+					});
+				} else {
+					alert('15명의 멘토를 설정해주세요');
+				}
+			});
+	$('#makeBtn').click(
+			function() {
+				if (count < 15) {
+					$('<div>').text(menteeMember[randomNumber[count++] + 14])
+							.attr('class', 'name').appendTo('.menteeBox');
+					console.log(count, randomNumber[count], menteeMember);
+				} else {
+					alert("멘토-멘티 매칭이 완료되었습니다.");
+				}
+			});
 };
